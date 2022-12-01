cb3ebca13c3bfc0ea528c95bd560ddc0104263d3
Nacho369 <nachodev369@gmail.com>
2022-12-01 04:45:26 -0800

    Task 2. How to make a script dynamic!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def main():
+    argv = sys.argv
+    n = 1
+
+    args_len = len(argv)
+
+    if (args_len == 0):
+        print("{} {}.".format(args_len - 1, "arguments"))
+    elif (args_len == 1):
+        print("{} {}:".format(args_len - 1, "argument"))
+    else:
+        print("{} {}:".format(args_len - 1, "arguments"))
+
+    while (n < args_len):
+        print("{} : {}".format(n, argv[n]))
+        n += 1
+
+
+if __name__ == '__main__':
+    main()
