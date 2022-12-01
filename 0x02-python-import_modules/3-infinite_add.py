96107ff254d1c04a50a918a9f3bb3949266b0ccd
Nacho369 <nachodev369@gmail.com>
2022-12-01 06:28:12 -0800

    Task 3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+    argv = sys.argv
+    arg_len = len(argv)
+    n = 1
+    sum_res = 0
+
+    while (n < arg_len):
+        sum_res += int(argv[n])
+        n += 1
+
+    print("{}".format(sum_res))
+    
+
+
+if __name__ == '__main__':
+    main()
