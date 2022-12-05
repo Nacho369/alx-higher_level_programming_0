0803396d5144726d918f88c10c155f8547603e09
Nacho369 <nachodev369@gmail.com>
2022-12-05 11:48:12 -0800

    Task 8. More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    first_char = ""
+
+    if sentence == "":
+        firt_char == None
+
+    first_char = sentence[0]
+    str_len = len(sentence)
+    new_tuple = (str_len, first_char)
+
+    return (new_tuple)
