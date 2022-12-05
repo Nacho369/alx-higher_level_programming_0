865ea032aafb5a13f54647ee7830e922d96b09ee
Nacho369 <nachodev369@gmail.com>
2022-12-05 14:19:05 -0800

    Task 8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,12 +3,9 @@
 
 def multiple_returns(sentence):
 
-    first_char = sentence[0]
-    str_len = len(sentence)
-
     if sentence == "":
         new_tuple = (0, None)
         return (new_tuple)
-    
-    new_tuple = (str_len, first_char)
+
+    new_tuple = (len(sentence), sentence[0])
     return (new_tuple)
