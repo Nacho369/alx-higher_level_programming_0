ba07b18c88a93223065d061c7399cea400881996
Nacho369 <nachodev369@gmail.com>
2022-12-07 12:40:26 -0800

    Task 1. Search and replace

---
+++ 0x04-python-more_data_structures/test_files/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for ind in range(len(new_list)):
+        if new_list[ind] == search:
+            new_list[ind] = replace
+
+    return (new_list)
