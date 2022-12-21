4b42c7acda708d6892ab35e57628ac3346c82e3e
Nacho369 <nachodev369@gmail.com>
2022-12-21 08:17:55 -0800

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """
+        Initialize a new square
+
+        Args:
+            size(int): Size of square
+        """
+        self.__size = size
