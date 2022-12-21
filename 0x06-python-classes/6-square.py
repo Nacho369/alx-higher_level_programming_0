90d5027bf0498d85849a5f93839e24b55e563fac
Nacho369 <nachodev369@gmail.com>
2022-12-21 12:43:08 -0800

    Task 6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,36 +31,32 @@ class Square:
 
     @property
     def position(self):
-        """Get the """
+        """Get the position of the square"""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """Set the position of the square"""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """ Prints to the stdout the square with the character "#" """
+        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
 
-        for e in range(self.__position[1]):
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
 
-        for x in range(self.__size):
-            for a in range(self.__position[0]):
-                print(" ", end="")
-
-            for y in range(self.__size):
-                print("#", end="")
-            print("")
