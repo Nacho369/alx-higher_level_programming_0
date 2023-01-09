f244758181e2fcd035a46d56958bcc9fbeac2c7c
Nacho369 <nachodev369@gmail.com>
2023-01-09 09:35:28 -0800

    Task 0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define the lookup() function"""
+
+def lookup(obj):
+    """The function returns the list of available attributes and
+    methods of an object:
+
+    Args:
+        obj: Object which attribute is to be returned
+
+    Return: A list object
+    """
+
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,5 @@
+# This directory contains files for Python - Inheritance
+
+## 0x0A. Python - Inheritance
+
+To be edited soon...
