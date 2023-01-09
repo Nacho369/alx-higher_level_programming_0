153448426d74ee15c6864066cba3e53644670125
Nacho369 <nachodev369@gmail.com>
2023-01-09 14:02:09 -0800

    Task 2. Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Define is_same_class() function to test if an object is an
+instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """The function checks if the object is exactly an instance
+    of the specified class
+
+    Args:
+        obj: Object to test
+        a_class: Class to compare object to
+    
+    Return: True if obj is an instance of a_class otherwise False
+    """
+    if type(obj) == a_class:
+        return True
+
+    return False
