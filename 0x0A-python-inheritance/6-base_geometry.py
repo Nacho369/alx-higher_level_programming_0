f9d8762fdce5d50a436f8c912acdef96593f75e4
Nacho369 <nachodev369@gmail.com>
2023-01-09 14:46:54 -0800

    Task 6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represent a BaseGeometry class"""
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
