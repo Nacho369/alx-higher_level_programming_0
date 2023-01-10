723778ff6690da3ef0a0808ed9ee91b944ecdd8f
Nacho369 <nachodev369@gmail.com>
2023-01-10 15:32:50 -0800

    Task 6. Create object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define the load_from_json_file() function"""
+
+import json
+
+def load_from_json_file(filename):
+    """
+    The function creates an Object from a “JSON file”
+
+    Args:
+        filename: File to read the json string from
+    """
+
+    with open(filename, "r", encoding="utf-8") as file_r:
+        return (json.load(file_r))
