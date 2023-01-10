7c5f320f5e3e27d65925084f808454f2dfc9d4ad
Nacho369 <nachodev369@gmail.com>
2023-01-10 14:45:37 -0800

    Task 4. From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define the from_json_string() function"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    The function returns an object (Python data structure)
+    represented by a JSON string
+
+    Args:
+        my_str: String to convert
+    """
+    return (json.loads(my_str))
