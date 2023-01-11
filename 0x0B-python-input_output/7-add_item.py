27b0dc07a1b80ae49df9626f2c028f374afa237c
Nacho369 <nachodev369@gmail.com>
2023-01-10 17:53:06 -0800

    Task 7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,13 +10,14 @@ load_from_json = __import__("6-load_from_json_file").load_from_json_file
 def add_item(args, filename):
     try:
         list_args = load_from_json(filename)
-    except:
+    except Exception:
         list_args = []
 
     for item in args:
         list_args.append(item)
 
     save_to_json(list_args, filename)
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
