b8b5575d30994045a732153b4f43708bc672faf4
Nacho369 <nachodev369@gmail.com>
2023-01-10 18:26:57 -0800

    Task 11. Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -37,10 +37,9 @@ class Student:
             my_dict = self.__dict__
         return (my_dict)
 
-
     def reload_from_json(self, json):
-        '''
+        """
             Replaces all attributes of student instance
-        '''
+        """
         for key, val in json.items():
             self.__setattr__(key, val)
