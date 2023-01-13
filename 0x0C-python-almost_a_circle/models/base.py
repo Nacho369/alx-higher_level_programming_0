a1833cdb1599618912b95e7d0c882905c71211f3
Nacho369 <nachodev369@gmail.com>
2023-01-13 03:26:06 -0800

    Task 1. Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Define a Base class"""
+
+
+class Base:
+    """Represnt a Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize the base class
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
