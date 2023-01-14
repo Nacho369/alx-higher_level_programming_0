ce287e1f35b5e9d01a08095ab5b4cccb98b401f9
Nacho369 <nachodev369@gmail.com>
2023-01-14 00:26:02 -0800

    Task 8. Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,4 +117,18 @@ class Rectangle(Base):
         """Prints a string to the stdout"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
-                                                       self.width, self.height)
+                                                       self.width,
+                                                       self.height)
+
+    def update(self, *args):
+        """
+        The function assigns an argument to each attribute
+        """
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
